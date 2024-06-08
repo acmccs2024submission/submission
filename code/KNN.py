aa8a70658dc1e68305cd176c4911bbf823daa31b
acmccs2024submission <168437467+acmccs2024submission@users.noreply.github.com>
2024-06-08 03:49:32 -0600

    Add files via upload

---
+++ code/KNN.py
@@ -0,0 +1,51 @@
+from CustomImageDataset import *
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+import numpy as np
+import time
+from sklearn.neighbors import KNeighborsClassifier
+
+if __name__ == "__main__":
+    srcdir ='WindowsGreyImage'
+    train_dataset = CustomImageDataset(annotations_file=r'annotations_train_2_50.1.csv',
+                                       weights_file=r'class_weights_2_50.1.csv', img_dir=r'tifs_grey_image',
+                                       transform=None)
+    test_dataset = CustomImageDataset(annotations_file=r'annotations_test_2_50.1.csv', img_dir=r'tifs_grey_image',
+                                      transform=None)
+    x_train = []
+    y_train =[]
+    start_time = time.time()
+    for image, label in train_dataset:
+        image = np.array(image.resize((512, 128))).flatten()
+        x_train.append(image)
+        y_train.append(label.item())
+    end_time = time.time()
+    execution_time = (end_time - start_time)/len(train_dataset)
+    print("Execution time per malware:", execution_time, "seconds")
+    x_test=[]
+    y_test=[]
+    for image, label in test_dataset:
+        image = np.array(image.resize((512, 128))).flatten()
+        x_test.append(image)
+        y_test.append(label.item())
+
+    # Create and fit the KNN classifier
+    clf = KNeighborsClassifier(1, weights='distance')
+
+    # Training
+    clf.fit(x_train, y_train)
+
+    # Predict labels for test data
+    start_time = time.time()
+    y_predict = clf.predict(x_test)
+    end_time = time.time()
+    execution_time = (end_time - start_time) / (len(test_dataset))
+    print("Execution time per malware:", execution_time, "seconds")
+    # Calculate accuracy
+    accuracy = accuracy_score(y_test, y_predict)
+    precision = precision_score(y_test, y_predict, average='weighted', zero_division=0)
+    recall = recall_score(y_test, y_predict, average='weighted', zero_division=0)
+    f1 = f1_score(y_test, y_predict, average='weighted', zero_division=0)
+    print(accuracy)
+    print(precision)
+    print(recall)
+    print(f1)
---
+++ code/cnn_bblock.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+from Trainer import *
+import time
+class cnn_bblock(nn.Module):
+    def __init__(self, num_classes=42):
+        super().__init__()
+
+        self.net = nn.Sequential(nn.LazyConv2d(out_channels=32, kernel_size=2),
+                                 nn.LazyConv2d(out_channels=32, kernel_size=3), nn.MaxPool2d(kernel_size=2, stride=2),nn.Dropout(0.5),
+                                 nn.LazyConv2d(out_channels=32, kernel_size=4), nn.MaxPool2d(kernel_size=2, stride=2), nn.Dropout(0.5),
+                                 nn.Flatten(), nn.LazyLinear(512), nn.Tanh(), nn.LazyLinear(256),nn.Tanh(),
+                                 nn.LazyLinear(num_classes), nn.Softmax())
+        self.net.apply(self.init_cnn)
+
+
+
+    def init_cnn(self, module):
+        """Initialize weights for CNNs.
+
+        Defined in :numref:`sec_lenet`"""
+        if type(module) == nn.Linear or type(module) == nn.Conv2d:
+            nn.init.xavier_uniform_(module.weight)
+
+    def layer_summary(self, X_shape):
+        """Defined in :numref:`sec_lenet`"""
+        X = torch.randn(*X_shape)
+        for layer in self.net:
+            X = layer(X)
+            print(layer.__class__.__name__, 'output shape:\t', X.shape)
+    def forward(self, X):
+        """Defined in :numref:`sec_linear_concise`"""
+        return self.net(X)
+
+
+#num_classes = 42
+#model = VGG_11(arch=((1, 64), (1, 128), (2, 256), (2, 512), (2, 512)), num_classes=num_classes).layer_summary((1, 3, 1024, 72))
+
+if __name__ == "__main__":
+    # Example usage:
+    # Define your model, dataloaders, criterion, optimizer, and device
+    num_classes = 42
+    model = cnn_bblock(num_classes=num_classes)
+
+    new_height = 128
+    new_width = 512
+    learning_rate = 0.01
+    transforms = transforms.Compose([
+        transforms.Resize((new_height, new_width)),
+        transforms.ToTensor()
+    ]
+    )
+
+    datamodule = CustomDataModule(batch_size=32, transform=transforms)
+    train_loader = datamodule.get_train_loader()
+    test_loader = datamodule.get_test_loader()
+    print(len(test_loader))
+
+    cost = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, )
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+    # ...
+    # Instantiate the Trainer class
+    trainer = Trainer(model, train_loader, test_loader, cost, optimizer, device)
+    # Train the model for a specified number of epochs
+    trainer.train(num_epochs=1)
+
+    start_time = time.time()
+    # Evaluate the model on the test set
+    test_accuracy = trainer.test()
+    print(f"Test Accuracy: {test_accuracy:.4f}")
+    end_time = time.time()
+    execution_time = (end_time - start_time) / (len(test_loader.dataset))
+    print("Execution time per malware:", execution_time, "seconds")
+
